dee4346e89c4f859edb53bdf4fe875a508f62556
Dáire McDermott <dairemcdermott@outlook.com>
2024-09-17 13:20:44 +0100

    initialises project

---
+++ src/main/java/work/mcdermott/moviepicker/MoviePickerApp.java
@@ -0,0 +1,15 @@
+package work.mcdermott.moviepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePickerApp
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MoviePickerApp.class, args);
+	}
+
+}
---
+++ src/test/java/work/mcdermott/moviepicker/MoviePickerAppTests.java
@@ -0,0 +1,15 @@
+package work.mcdermott.moviepicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePickerAppTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
